620c623635df6df445d5738fd7b1518c034105e1
farooq <faroooq.in@gmail.com>
2020-10-14 19:41:08 +0530

    form enhancements

--- src/app/course-enroll-form/course-enroll-form.component.ts
+++ src/app/course-enroll-form/course-enroll-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -14,43 +14,54 @@ import { startWith, map } from 'rxjs/operators';
 export class CourseEnrollFormComponent implements OnInit {
 
   userList: AngularFireList<any>;
-
   results: Observable<string[]>;
   data: any = ['Angular', 'React', 'Javascript', 'CSS', 'Html', 'Java', 'Python', 'Ionic'];
-
+  buttonValue: string = "Please fill all the details";
   registerForm: FormGroup;
+  disabledSubmitButton: boolean = true;
+
+  @HostListener('input') oninput() {
+
+    if (this.registerForm.valid) {
+      this.disabledSubmitButton = false;
+      this.buttonValue = "Enroll NOW"
+    }
+  }
 
   constructor(
     private router: Router,
     public db: AngularFireDatabase,
-    private toastrService: ToastService) {
+    private toastrService: ToastService,
+    private fb: FormBuilder) {
+
     this.userList = db.list('enrolled-students')
+
+    this.registerForm = fb.group({
+      'firstName': ['', Validators.required],
+      'lastName': ['', Validators.required],
+      'email': ['', Validators.compose([Validators.required, Validators.email])],
+      'course': ['', Validators.required],
+      'phone': ['', Validators.required],
+      'onetoone': [''],
+      'subscription': [''],
+    });
   }
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      course: new FormControl(),
-      phone: new FormControl(),
-      onetoone: new FormControl(),
-      subscription: new FormControl()
-    });
-
     this.results = this.registerForm.controls.course.valueChanges
       .pipe(
-        startWith('red'),
+        startWith(''),
         map((value: string) => this.filter(value))
       );
   }
 
   filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
+    const filterValue = value;
     return this.data.filter((item: any) => item.toLowerCase().includes(filterValue));
   }
 
   onSubmit() {
+    this.disabledSubmitButton = true;
     const response = this.userList.push({
       firstName: this.registerForm.value.firstName,
       lastName: this.registerForm.value.lastName,
@@ -60,22 +71,11 @@ export class CourseEnrollFormComponent implements OnInit {
       onetoone: this.registerForm.value.onetoone,
       subscription: this.registerForm.value.subscription
     })
-    if(response != null) {
-      const options = { opacity: 1, extendedTimeOut: 10000  };
-      this.toastrService.success( '', 'You have successfully enrolled to ' + this.registerForm.value.course + ' course', options);
-      // this.userList = AngularFireList<any>
-      
-      this.registerForm = new FormGroup({
-        firstName: new FormControl(),
-        lastName: new FormControl(),
-        email: new FormControl(),
-        course: new FormControl(),
-        phone: new FormControl(),
-        onetoone: new FormControl(),
-        subscription: new FormControl()
-      });
+    if (response != null) {
+      const options = { opacity: 1, extendedTimeOut: 10000 };
+      this.toastrService.success('', 'You have successfully enrolled to ' + this.registerForm.value.course + ' course', options);
+      this.registerForm.reset();
+      this.router.navigateByUrl('/');
     }
   }
-
-
 }
